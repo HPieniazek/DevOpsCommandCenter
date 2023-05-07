ad2180ab5d5588077870ca8fb38e299b62a0db6a
HPieniazek <pieniazekh@gmail.com>
2023-05-07 10:38:36 +0200

    machines Api

---
+++ devops_command_center_api/DevOpsCommandCenterAPI/DevOpsCommandCenterAPI/Controllers/MachinesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DevOpsCommandCenterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MachinesController : ControllerBase
+    {
+        // GET: api/<MachinesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MachinesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MachinesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MachinesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MachinesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ devops_command_center_api/DevOpsCommandCenterAPI/DevOpsCommandCenterAPI/Machine.cs
@@ -0,0 +1,15 @@
+﻿namespace DevOpsCommandCenterAPI
+{
+    public class Machine
+    {
+        public string Name { get; set; }
+        
+        public List<string> Commands { get; set; }
+
+        public string Version { get; set; }
+
+        public string Description { get; set; }
+        public Machine() { }
+
+    }
+}
