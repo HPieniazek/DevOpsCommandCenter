f2f50ba1f81130f560dc469b6a0e873f9b42e78e
HPieniazek <pieniazekh@gmail.com>
2023-06-14 23:38:41 +0200

    Init

---
+++ devops_command_center_api/AuthenticationAPI/AuthenticationAPI/Controllers/MailController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AuthenticationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailController : ControllerBase
+    {
+        // GET: api/<MailController>
+        [HttpGet]
+        public IActionResult Get([FromBody] JObject data, [FromHeader(Name="Authorizatuion")] string token, [FromHeader(Name = "Username")] string username)
+        {
+            if (string.IsNullOrWhiteSpace(token) || !AuthController.IsTokenValid(username, token))
+            {
+                return Unauthorized();
+            }
+
+            // Teraz możemy przetworzyć dane, ponieważ wiemy, że token jest poprawny
+            // Na przykład możemy po prostu zwrócić je w odpowiedzi
+            return Ok(data);
+        }
+
+        //// GET api/<MailController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<MailController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<MailController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<MailController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ devops_command_center_api/AuthenticationAPI/AuthenticationAPI/Models/AuthModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+public class AuthModel
+{
+    [Required]
+    public string? Token { get; set; }
+
+    [Required]
+    public string? User { get; set;}
+
+}
+
